32314b898c3cbc852c3a50a9f245105a1d76f6ae
XuanPhat0603 <nguyenxuanphat060301@gmail.com>
2023-01-15 23:14:49 +0700

    fix

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../../components/Auth/LoginForm';
+import LoginForm from '../../components/auth/LoginForm';
 
 export default function Login() {
   return <LoginForm />;
