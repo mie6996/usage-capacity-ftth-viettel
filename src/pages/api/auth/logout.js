a45952bfb46192af89a98eb417a9d27611ee4a89
XuanPhat0603 <nguyenxuanphat060301@gmail.com>
2023-01-16 05:23:23 +0700

    remove cookie

--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -1,22 +1,9 @@
-import { serialize } from 'cookie';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res
       .status(405)
       .json({ success: false, message: 'Method not allowed' });
   }
-
-  res.setHeader(
-    'Set-Cookie',
-    serialize('token', '', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: -1,
-      path: '/',
-    })
-  );
 
   res.status(200).json({ success: true, message: 'Successfuly logged out!' });
 }
